c41a0f0ec59b459e7f387d520de8692e4f90ee89
Ambsmith231 <ambsmith396@gmail.com>
2025-12-15 15:40:20 +0000

    work on the main.js file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp } from 'vuehttps://unpkg.com/vue@3/dist/vue.esm-browser.js'
 import App from './App.vue'
 import router from './router/index.js'
 
